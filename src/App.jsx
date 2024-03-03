cc2f4bac75358e0bf2904acec851e076e3241532
Vikash2048 <rpsvikash16@gmail.com>
2024-03-03 17:13:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+
+
+  return (
+    <div className='w-[400px] h-[300px] md:bg-slate-950 md:text-white '>
+      hello world
+    </div>
+  )
+}
+
+export default App
