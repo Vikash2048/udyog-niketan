a94680e48978191e176a2e4c44c3b008c10b19b2
vikasrajput47 <vikasrajpoot479@gmail.com>
2024-03-17 07:50:04 +0530

    added image for the contact form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-
+import img from "../../public/images/contact.jpg";
 
 // taking detail for contact form
 const Contact = () => {
@@ -9,6 +9,7 @@ const Contact = () => {
     email: "",
     message: "",
   });
+
   const submitHandler = (e) => {
     e.preventDefault();
     console.log(data);
@@ -27,64 +28,71 @@ const Contact = () => {
       ...prevData,
       [name]: value,
     }));
-    const email = "vikasrajpoot974@gmail.com";
-    const subject = "subject";
-    const body = JSON.stringify(data);
-    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
   };
+
   return (
-    <div id="Contact" className="mt-10  ">
-      <form
-        onSubmit={submitHandler}
-        className="flex flex-col justify-center items-center bg-slate-100 p-8 rounded-lg"
+    <>
+      <h2 className="text-[#FF6B18] mt-20 text-center  font-medium text-lg pb-6 ">
+        Contact Us
+      </h2>
+      <div
+        id="Contact"
+        className="flex flex-col md:flex-row justify-around items-center "
       >
-        <h2 className="text-[#FF6B18] font-medium text-lg pb-16 ">
-          Contact Us
-        </h2>
-        <div className="flex justify-start w-full mb-4   ">
-          <input
-            type="text"
-            placeholder="Enter First Name"
-            className="border  p-2 w-1/2"
-            value={data.firstName}
-            name="firstName"
-            onChange={dataInput}
-          />
-          <input
-            type="text"
-            placeholder="Enter Last Name"
-            className="border  p-2 w-1/2 "
-            name="lastName"
-            value={data.lastName}
-            onChange={dataInput}
-          />
+        <div className="md:w-1/2 max-w-lg ml-4">
+          <img src={img} alt="Contact" className="w-full h-auto rounded-lg" />
         </div>
-        <input
-          type="email"
-          placeholder="Enter your Email"
-          required
-          value={data.email}
-          name="email"
-          onChange={dataInput}
-          className="border  p-2 w-full"
-        />
-        <textarea
-          name="message"
-          cols="30"
-          rows="10"
-          value={data.message}
-          onChange={dataInput}
-          placeholder="Message"
-          className=" p-2 w-full mt-4"
-        ></textarea>
-        <button
-          type="submit"
-          className="bg-red-600 text-white px-4 py-2 mt-4 rounded hover:bg-red-700"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
+        <div className="md:w-1/2 max-w-lg mr-4">
+          <form
+            onSubmit={submitHandler}
+            className="flex flex-col justify-center items-center border p-8 rounded-lg shadow-md shadow-orange-100"
+          >
+            <div className="flex justify-start w-full mb-4">
+              <input
+                type="text"
+                placeholder="Enter First Name"
+                className="border p-2 w-1/2 mr-2"
+                value={data.firstName}
+                name="firstName"
+                onChange={dataInput}
+              />
+              <input
+                type="text"
+                placeholder="Enter Last Name"
+                className="border p-2 w-1/2"
+                name="lastName"
+                value={data.lastName}
+                onChange={dataInput}
+              />
+            </div>
+            <input
+              type="email"
+              placeholder="Enter your Email"
+              required
+              value={data.email}
+              name="email"
+              onChange={dataInput}
+              className="border p-2 w-full mb-4"
+            />
+            <textarea
+              name="message"
+              cols="30"
+              rows="5"
+              value={data.message}
+              onChange={dataInput}
+              placeholder="Message"
+              className="border p-2 w-full mb-4"
+            ></textarea>
+            <button
+              type="submit"
+              className="bg-red-600 text-white px-4 py-2 rounded hover:bg-red-700"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </>
   );
 };
 
